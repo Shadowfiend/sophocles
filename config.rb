18aa69d8f15dcd030954feed3d1a0ce78d75f800
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2012-04-03 01:29:02 -0400

    Add Susyed SASS for a starting poing.

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'susy'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+http_images_path = '/images'
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
